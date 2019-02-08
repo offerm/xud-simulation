97ac2040434861fb70b816f7b5bcdafa55687d9a
Moshe Shababo <dicentralized@gmail.com>
2019-02-08 12:33:26 +0200

    Use a custom $GOPATH for lnd build

--- install.sh
+++ install.sh
@@ -4,40 +4,50 @@ if ! mkdir xud
 then
      echo "xud already installed"
 else
-    echo "installing xud..."
+    echo "starting xud clone..."
     if ! git clone https://github.com/ExchangeUnion/xud.git > /dev/null 2>&1; then
        echo "unable to git clone xud"
        exit 1
     fi
-    echo "xud downloaded"
+    echo "xud clone finished"
+
+    echo "starting xud npm install..."
     cd xud
     if ! npm install > /dev/null 2>&1 ; then
         echo "unable to npm install xud"
         exit 1
     fi
-    echo "xud npm installed"
+    echo "finished xud npm install"
+
+    echo "starting xud npm compile..."
     if ! npm run compile > /dev/null 2>&1 ; then
         echo "unable to build xud"
         exit 1
     fi
-    echo "xud installed"
+    echo "finished xud npm compile"
     cd ..
 fi
 
-if ! mkdir lnd
+if ! mkdir go
 then
-     echo "lnd already installed"
+    echo "lnd already installed"
 else
-    echo "installing lnd..."
-    if ! git clone -b resolver+simnet-ltcd https://github.com/ExchangeUnion/lnd.git ./lnd > /dev/null 2>&1; then
+    export GOPATH=~/go/src/github.com/ExchangeUnion/xud-simulation/go
+
+    echo "starting lnd clone..."
+    if ! git clone -b resolver+simnet-ltcd https://github.com/ExchangeUnion/lnd.git  $GOPATH/src/github.com/lightningnetwork/lnd > /dev/null 2>&1; then
        echo "unable to git clone lnd"
        exit 1
     fi
-    echo "lnd downloaded"
-    cd lnd
+    echo "finished lnd clone"
+
+    echo "starting lnd make..."
+    cd go/src/github.com/lightningnetwork/lnd
     if ! make > /dev/null 2>&1 ; then
         echo "unable to make lnd"
         exit 1
     fi
-    echo "lnd installed"
+    echo "finished lnd make"
+
+    cd ../../../../../
 fi
